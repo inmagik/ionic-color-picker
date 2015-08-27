b24e48f900e0e26c316412a438e0cb77b5854207
Mauro Bianchi <bianchimro@gmail.com>
2015-08-27 10:26:48 +0200

    cleaned gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,10 @@
 var gulp = require('gulp');
-//var bower = require('bower');
 var concat = require('gulp-concat');
-//var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
-//var sh = require('shelljs');
 var addsrc = require('gulp-add-src');
 var replace = require('gulp-replace');
 var jsifyTemplates = require('gulp-jsify-html-templates');
-//var cat  = require('gulp-cat'); 
 var uglify = require('gulp-uglify');
 var order = require("gulp-order");
 
@@ -25,11 +21,9 @@ gulp.task('color-picker', function() {
     .pipe(addsrc( './src/ionic-color-picker.js'))
     .pipe(order(['templates.js', 'src/ionic-color-picker.js']))
     .pipe(concat('ionic-color-picker.js'))
-    //.pipe(gulp.dest( 'ColorPickerExample/www/lib/ionic-color-picker/' ) )
     .pipe(gulp.dest('./dist/'))
     .pipe(uglify({mangle:false}))
     .pipe(rename({ extname: '.min.js' }))
-    //.pipe(gulp.dest('ColorPickerExample/www/lib/ionic-color-picker/'))
     .pipe(gulp.dest('./dist/'))
     
 
@@ -37,13 +31,11 @@ gulp.task('color-picker', function() {
 
 gulp.task('css-color-picker', function() {
     return gulp.src( './src/ionic-color-picker.css')
-    //.pipe(gulp.dest( 'ColorPickerExample/www/lib/ionic-color-picker' ) )
     .pipe(gulp.dest('./dist/'))
     .pipe(minifyCss({
       keepSpecialComments: 0
     }))
     .pipe(rename({ extname: '.min.css' }))
-    //.pipe( gulp.dest( 'ColorPickerExample/www/lib/ionic-color-picker' ) )
     .pipe(gulp.dest('./dist/'))
 
 });
